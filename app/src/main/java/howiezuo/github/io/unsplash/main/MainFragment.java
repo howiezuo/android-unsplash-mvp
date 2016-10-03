33930ece689da4353f3580ca0c674872e558c253
Howie Zuo <howie.zuo@gmail.com>
2016-10-02 19:10:37 -0700

    ripple

--- app/src/main/java/howiezuo/github/io/unsplash/main/MainFragment.java
+++ app/src/main/java/howiezuo/github/io/unsplash/main/MainFragment.java
@@ -45,14 +45,6 @@ public class MainFragment extends BaseFragment implements MainContract.View {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        isLoading = true;
-        mPresenter.loadPhotos();
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
@@ -76,6 +68,14 @@ public class MainFragment extends BaseFragment implements MainContract.View {
                 }
             }
         });
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        isLoading = true;
+        mPresenter.loadPhotos();
     }
 
     @Override
