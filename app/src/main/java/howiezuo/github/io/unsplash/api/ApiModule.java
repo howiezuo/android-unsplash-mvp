0d26d42505aeb2e121a7d0d790b62c7e62addeb3
Howie Zuo <howie.zuo@gmail.com>
2016-10-19 16:31:37 +0900

    fix

--- app/src/main/java/howiezuo/github/io/unsplash/api/ApiModule.java
+++ app/src/main/java/howiezuo/github/io/unsplash/api/ApiModule.java
@@ -1,10 +1,15 @@
 package howiezuo.github.io.unsplash.api;
 
+import java.io.IOException;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -15,9 +20,25 @@ public class ApiModule {
 
     @Provides
     @Singleton
-    OkHttpClient provideOkHttpClient() {
+    Interceptor provideHttpInterceptor() {
+        return new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request original = chain.request();
+                Request request = original.newBuilder()
+                        .addHeader("Accept-Version", "v1")
+                        .addHeader("Authorization", "Client-ID bb0b8493dba89c5b8765bdca724c00c3766b20971017f3315536ba19856e6287")
+                        .build();
+                return chain.proceed(request);
+            }
+        };
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient(Interceptor interceptor) {
         return new OkHttpClient.Builder()
-                .addInterceptor(new HttpInterceptor())
+                .addInterceptor(interceptor)
                 .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
                 .build();
     }
